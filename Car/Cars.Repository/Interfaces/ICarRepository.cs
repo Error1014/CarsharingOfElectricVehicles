dd35e00fdc16bbb36c58608faa80e9363fb85611
the Error1024  was busy <kourov1024@gmail.com>
2023-04-21 13:05:47 +0300

    Repository для микросервиса Car

---
+++ Car/Cars.Repository/Interfaces/ICarRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cars.Repository.Entities;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Repository.Interfaces
+{
+    public interface ICarRepository : IRepository<Car, Guid>
+    {
+    }
+}
---
+++ Car/Cars.Repository/Repositories/CarRepository.cs
@@ -0,0 +1,19 @@
+﻿using Cars.Repository.Entities;
+using Cars.Repository.Interfaces;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Repository.Repositories
+{
+    public class CarRepository : Repository<Car, Guid>, ICarRepository
+    {
+        public CarRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
