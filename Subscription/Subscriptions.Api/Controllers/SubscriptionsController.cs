bdb22e3e398a4c43eedf0131e2141e864242e254
Error1014 <kourov1024@gmail.com>
2023-05-23 12:16:07 +0300

    Рефактринг Subscription

--- Subscription/Subscriptions.Api/Controllers/SubscriptionsController.cs
+++ Subscription/Subscriptions.Api/Controllers/SubscriptionsController.cs
@@ -23,7 +23,7 @@ namespace Subscriptions.Api.Controllers
             var result = await _subscriptionService.GetActualSubscription();
             return Ok(result);
         }
-        [HttpGet("{id}")]
+        [HttpGet("{/id}")]
         public async Task<IActionResult> GetSubscription(Guid id)
         {
             var result = await _subscriptionService.GetSubscription(id);
@@ -43,14 +43,14 @@ namespace Subscriptions.Api.Controllers
             return Ok();
         }
         [RoleAuthorize("Admin")]
-        [HttpPut]
+        [HttpPut("{/id}")]
         public async Task<IActionResult> UpdateSubscription(Guid id, [FromQuery] SubscriptionDTO subscriptionDTO)
         {
             await _subscriptionService.UpdateSubscripton(id, subscriptionDTO);
             return Ok();
         }
         [RoleAuthorize("Admin")]
-        [HttpDelete]
+        [HttpDelete("{/id}")]
         public async Task<IActionResult> RemoveSubscription(Guid id)
         {
             await _subscriptionService.RemoveSubscription(id);
