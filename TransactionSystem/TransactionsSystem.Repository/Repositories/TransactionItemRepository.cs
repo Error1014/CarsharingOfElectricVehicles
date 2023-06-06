bae1a27832206e6f841bb3201d8deeb0f3aea87a
Error1014 <kourov1024@gmail.com>
2023-06-06 14:41:21 +0300

    Репозиторий микросервиса

---
+++ TransactionSystem/TransactionsSystem.Repository/Repositories/TransactionItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionsSystem.Repository.Entities;
+using TransactionsSystem.Repository.Interfaces;
+
+namespace TransactionsSystem.Repository.Repositories
+{
+    public class TransactionItemRepository : Repository<TransactionItem, Guid>, ITransactionItemRepository
+    {
+        public TransactionItemRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ TransactionSystem/TransactionsSystem.Repository/Repositories/TypeTransactionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionsSystem.Repository.Entities;
+using TransactionsSystem.Repository.Interfaces;
+
+namespace TransactionsSystem.Repository.Repositories
+{
+    public class TypeTransactionRepository : Repository<TypeTransaction, int>, ITypeTransactionRepository
+    {
+        public TypeTransactionRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
