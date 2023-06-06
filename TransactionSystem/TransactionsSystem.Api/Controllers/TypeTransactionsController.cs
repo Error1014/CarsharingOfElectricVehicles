db52d9127b1312572ce6575936b0bd82673d69b8
Error1014 <kourov1024@gmail.com>
2023-06-06 16:02:58 +0300

    Котролер TypeTransactionsController

---
+++ TransactionSystem/TransactionsSystem.Api/Controllers/TypeTransactionsController.cs
@@ -0,0 +1,51 @@
+﻿using Infrastructure.Attributes;
+using Infrastructure.HelperModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TransactionsSystem.Service.Interfaces;
+
+namespace TransactionsSystem.Api.Controllers
+{
+    public class TypeTransactionsController : BaseApiController
+    {
+        private readonly ITypeTransactionService _transactionService;
+
+        public TypeTransactionsController(ITypeTransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetType(int id)
+        {
+            var result = await _transactionService.GetTypeTransaction(id);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTypes()
+        {
+            var result = await _transactionService.GetTypeTransactions();
+            return Ok(result);
+        }
+        [RoleAuthorize("Admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddType(string value)
+        {
+            await _transactionService.AddTypeTransaction(value);
+            return Ok();
+        }
+        [RoleAuthorize("Admin")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateType(int id, string value)
+        {
+            await _transactionService.UpdateTypeTransaction(id, value);
+            return Ok();
+        }
+        [RoleAuthorize("Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveType(int id)
+        {
+            await _transactionService.RemoveTypeTransiction(id);
+            return Ok();
+        }
+    }
+}
--- TransactionSystem/TransactionsSystem.Api/Program.cs
+++ TransactionSystem/TransactionsSystem.Api/Program.cs
@@ -1,5 +1,12 @@
 using TransactionsSystem.Repository.Context;
 using Infrastructure.Extensions;
+using Infrastructure.DTO;
+using Infrastructure.Interfaces;
+using TransactionsSystem.Repository.Interfaces;
+using TransactionsSystem.Repository.Repositories;
+using TransactionsSystem.Service.Interfaces;
+using TransactionsSystem.Service.Service;
+using TransactionsSystem.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +15,15 @@ builder.Services.RegistrationDbContext<TransactionContext>(builder.Configuration
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddScoped<IUnitOfWork, UnitOfWork>()
+    .AddScoped<ITypeTransactionService, TypeTransactionService>()
+    .AddScoped<ITransactionService, TransactionService>();
+builder.Services.AddScoped<UserSession>();
+builder.Services.AddScoped<IUserSessionGetter>(serv => serv.GetRequiredService<UserSession>());
+builder.Services.AddScoped<IUserSessionSetter>(serv => serv.GetRequiredService<UserSession>());
 
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
