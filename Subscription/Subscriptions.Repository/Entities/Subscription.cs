a85b684084535ab79c3f444e7d9a1a84f62b3bce
Error1014 <kourov1024@gmail.com>
2023-06-13 02:38:25 +0300

    Исправление БД подписки

--- Subscription/Subscriptions.Repository/Entities/Subscription.cs
+++ Subscription/Subscriptions.Repository/Entities/Subscription.cs
@@ -4,6 +4,8 @@ namespace Subscriptions.Repository.Entities
 {
     public class Subscription : BaseEntity<Guid>
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
         public int QuantityMinutsInDay { get; set; }
         public decimal Price { get; set; }
     }
